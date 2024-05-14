3a5ced3d2bfef0a1aea5b9fff1ed814d6c75ff69
Oscar Jacquet <153740216+Oscarj-jqt@users.noreply.github.com>
2024-05-14 10:45:25 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,4 @@
+<?php include './header.php'; ?>
+<h1>A propos</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, praesentium! Voluptate rerum soluta eaque facilis aperiam sequi et, molestiae quidem reprehenderit. Magnam aut perferendis autem dolor minus harum necessitatibus mollitia?</p>
+<?php include './footer.php'; ?>
